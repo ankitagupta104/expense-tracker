607b5c3863ecd64e4cbe1e151493293d20f56195
ankitagupta104 <ankitaguptanitrr@gmail.com>
2025-06-04 14:18:51 +0530

    Update apiPaths.js new server

--- expense-tracker/src/utils/apiPaths.js
+++ expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://expense-tracker-api-jade.vercel.app";
+export const BASE_URL = "https://expense-tracker-poeg.onrender.com/";
 
 //utils/apiPaths.js
 export const API_PATHS = {
